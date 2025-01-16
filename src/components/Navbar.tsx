9d9360b53ea6442174e57a5f3b8d59d405f93a36
TamirCode <TamirMailing@gmail.com>
2025-01-16 23:38:16 +0200

    e

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,10 @@
+"use client"
+
 import Link from 'next/link'
 import MobileMenu from './MobileMenu'
 import Image from 'next/image'
+import { ClerkLoaded, ClerkLoading } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react'
 
 export default function Navbar() {
 
@@ -35,7 +39,31 @@ export default function Navbar() {
 
 			{/* RIGHT */}
 			<div className='w-[30%] flex items-center gap-4 xl:gap-8 justify-end'>
-				
+				<ClerkLoading>
+					{/* loading spinner */}
+					<div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]" role="status"><span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
+					</div>
+				</ClerkLoading>
+				<ClerkLoaded>
+					<SignedIn>
+						<div className="cursor-pointer">
+							<Image src="/people.png" alt="" width={20} height={20} />
+						</div>
+						<div className="cursor-pointer">
+							<Image src="/messages.png" alt="" width={20} height={20} />
+						</div>
+						<div className="cursor-pointer">
+							<Image src="/notifications.png" alt="" width={20} height={20} />
+						</div>
+						<UserButton />
+					</SignedIn>
+					<SignedOut>
+						<div>
+							<Image src="/login.png" alt="" width={20} height={20} />
+							<Link href="/sign-in">Login/Register</Link>
+						</div>
+					</SignedOut>
+				</ClerkLoaded>
 				<MobileMenu />
 			</div>
 
